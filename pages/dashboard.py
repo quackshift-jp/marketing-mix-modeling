d5cb2021f4910a57ce5d1f1e94f0acf488fb0335
yukihirano0425 <yuki.hirano@uzabase.com>
2023-11-10 23:58:08 +0900

    change:prophetモジュールをページ部分に追加する

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import streamlit as st
 
+from services import prophet
 from services.utils import read_dataset
 
 
@@ -10,11 +11,17 @@ def display():
     upload_file = st.file_uploader("Choose a CSV file", type=["csv"])
     if upload_file:
         try:
-            df = read_dataset.read_data(upload_file, "Date")
-
-            plot_cost_and_revenue(df, "date", ["sales"], df.columns[2:-1])
+            mmm_df = read_dataset.read_data(upload_file, "Date")
+            st.subheader("売上とコストの可視化", divider="rainbow")
+            plot_cost_and_revenue(mmm_df, "date", ["sales"], mmm_df.columns[2:])
         except ValueError:
             st.error("データの日付は「Dateまたはdate」、売上は「Salesまたはsales」にしてください。")
+
+        pred, prophet_model = prophet.fit_predict_prophet_model(mmm_df)
+        df_with_prophet = prophet.extract_prophet_data(
+            pred, mmm_df, target_prophet_cols=["trend", "yearly"]
+        )
+        # st.table(df_with_prophet)
 
 
 def plot_cost_and_revenue(
