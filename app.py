40bbfb18f433f9d3160d3dcba211d8fe8df829a5
yukihirano0425 <yuki.hirano@uzabase.com>
2023-11-13 10:17:44 +0900

    changeUIを整える・適宜入力ボタンを設置する

--- app.py
+++ app.py
@@ -5,7 +5,10 @@ from pages import dashboard
 
 def app():
     st.title("Marketing Mix Modeling")
-    dashboard.display()
+    st.sidebar.markdown("### 機械学習に用いるcsvファイルを入力してください")
+    upload_file = st.sidebar.file_uploader("Upload a CSV file", type=["csv"])
+    if upload_file:
+        dashboard.display(upload_file)
 
 
 if __name__ == "__main__":
